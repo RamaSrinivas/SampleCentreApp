708da87a7f09601fa598835d323f1ff09f3ee160
RamaSrinivasM <srinivas.m@way2online.co.in>
2019-07-02 14:49:52 +0530

    second commit

--- app/src/main/java/com/way/samplecentreapp/MainActivity.java
+++ app/src/main/java/com/way/samplecentreapp/MainActivity.java
@@ -2,12 +2,38 @@ package com.way.samplecentreapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.centre.CentreApp;
+import com.centre.CentreListeners.CentreCallbacks;
+
+public class MainActivity extends AppCompatActivity  implements CentreCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Replace with your keys
+        CentreApp.initialize(MainActivity.this, "YOUR_API_KEY",  "USER_GOOGLE_ADVERTISER_ID");
+        CentreApp.start("testing transaction");
+        CentreApp.addTrait("string", "name", "myname");
+        CentreApp.addTrait("string", "email", "myemail@gmail.com");
+        CentreApp.addTrait("date", "stratdate", "20192233");
+        CentreApp.send();
+
+    }
+
+    @Override
+    public void onDataSuccess(String message, String data) {
+        Log.i("onDataSuccess",""+data);
+        Toast.makeText(this,message.toString(),2000).show();
+
+    }
+
+    @Override
+    public void onDataFailure(String message) {
+        Log.i("onDataFailure",""+message);
+        Toast.makeText(this,message.toString(),2000).show();
     }
 }
